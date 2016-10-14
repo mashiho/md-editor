d2a59fd9d442f2f8a473237670f7adda9b0c267e
Kentaro Matsushita <kentaro.m811@gmail.com>
2016-10-14 21:49:27 +0900

    Add application menu.

--- main.js
+++ main.js
@@ -1,14 +1,55 @@
 const electron = require('electron');
 
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const {
+  app,
+  BrowserWindow,
+  Menu,
+} = electron;
 
 let mainWindow;
+
+const menu = Menu.buildFromTemplate([
+  {
+    label: 'Markdown editor',
+    submenu: [
+      { label: 'About' },
+      { label: 'Quit' },
+    ],
+  },
+  {
+    label: 'File',
+    submenu: [
+      { label: 'New File' },
+      { label: 'Save' },
+    ],
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      { label: 'Copy', accelerator: 'Command+C', selector: 'copy' },
+      { label: 'Paste', accelerator: 'Command+V', selector: 'paste' },
+    ],
+  },
+  {
+    label: 'Develop',
+    submenu: [
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: 'Alt+Command+I',
+        click: function () {
+          mainWindow.toggleDevTools();
+        },
+      },
+    ],
+  },
+]);
 
 function createWindow() {
   const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
   mainWindow = new BrowserWindow({ left: 0, top: 0, width: width, height: height });
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  Menu.setApplicationMenu(menu);
 
   // NOTE: デバッグ用
   // mainWindow.webContents.openDevTools();
