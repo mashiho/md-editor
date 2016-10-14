d47bdfdb3d0e363eddf89af31aee9762274ce8b8
Kentaro Matsushita <kentaro.m811@gmail.com>
2016-10-14 20:51:32 +0900

    Fix gulpfile.js.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,16 @@
 import babelify from 'babelify';
 import gulp from 'gulp';
+import plumber from 'gulp-plumber';
 import browserify from 'browserify';
+import watchify from 'watchify';
 import source from 'vinyl-source-stream';
 
+// TODO: エラーをキャッチしたときにログを出すようにする
 gulp.task('build', () => {
-  browserify({ entries: ['app.js'] })
+  watchify(browserify({ entries: ['app.js'] }))
   .transform(babelify)
   .bundle()
+  .pipe(plumber())
   .pipe(source('bundle.js'))
   .pipe(gulp.dest('./public'));
 });
