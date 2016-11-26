f812350f9f317cf58be6a4a362810c7fe3351293
Kentaro Matsushita <kentaro.m811@gmail.com>
2016-11-26 20:51:30 +0900

    Fix app_spec.js

--- test/app_spec.js
+++ test/app_spec.js
@@ -6,11 +6,23 @@ import Preview from '../components/preview';
 import { assert, expect } from 'chai';
 import { shallow, mount } from 'enzyme';
 import sinon from 'sinon';
+import marked from 'marked';
 
 describe('Appコンポーネントのテスト', () => {
   it('<Editor />と<Preview />がレンダリングされていること', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find(Editor)).to.have.length(1);
     expect(wrapper.find(Preview)).to.have.length(1);
+  });
+
+  it('<Editor />で入力した文字を取得できること', () => {
+    const updateText = sinon.spy(Editor.prototype, 'updateText');
+    const wrapper = shallow(<Editor onChange={updateText} text={'Hello World!'} />);
+    expect(wrapper.props('text').value).to.equal('Hello World!');
+  });
+
+  it('<Preview />でマークダウン記法の文字列をHTMLに変換できること', () => {
+    const wrapper = mount(<Preview markdown={'**Hello World!**'} />);
+    expect(marked(wrapper.prop('markdown'))).to.match(/<strong>Hello World!<\/strong>/);
   });
 });
